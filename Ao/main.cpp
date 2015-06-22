1d76842a58b3c11706fe2de2f813248f49942041
Sean Mahbod <smahbod2014@gmail.com>
2015-06-21 20:56:03 -0700

    Incorporated FreeType (maybe)

--- Ao/main.cpp
+++ Ao/main.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #include <glm/gtx/transform.hpp>
-#include "Window.h"
-#include "Shader.h"
-#include "Layer.h"
-#include "BatchRenderer2D.h"
-#include "TextureManager.h"
-#include "Sprite.h"
-#include "Group.h"
+#include "src/graphics/Window.h"
+#include "src/graphics/Shader.h"
+#include "src/graphics/Layer.h"
+#include "src/graphics/BatchRenderer2D.h"
+#include "src/managers/TextureManager.h"
+#include "src/graphics/Sprite.h"
+#include "src/graphics/Group.h"
 #include <ctime>
 
 int main(int argc, char* argv[])
 {
 	srand((unsigned int)time(NULL));
 
-	float MAX_FPS = 120.0f;
+	float MAX_FPS = 1000.0f;
 	Window window("Ao", 960, 540);
 	window.setMaxFPS(MAX_FPS);
 
-	TextureManager::loadTexture("face", "Textures/wtf face.png");
-	TextureManager::loadTexture("whocares", "Textures/whocares.png");
-	TextureManager::loadTexture("bricks", "Textures/bricks.png");
-	TextureManager::loadTexture("checker", "Textures/checkerboard.png");
+	TextureManager::loadTexture("face", "Resources/Textures/wtf face.png");
+	TextureManager::loadTexture("whocares", "Resources/Textures/whocares.png");
+	TextureManager::loadTexture("bricks", "Resources/Textures/bricks.png");
+	TextureManager::loadTexture("checker", "Resources/Textures/checkerboard.png");
 
 	Shader* shader = new Shader();
-	shader->load("Shaders/Basic.vert", "Shaders/Basic.frag");
+	shader->load("Resources/Shaders/Basic.vert", "Resources/Shaders/Basic.frag");
 
 	BatchRenderer2D* renderer = new BatchRenderer2D();
 
 	Layer layer(renderer, shader, glm::ortho<float>(-16.0f, 16.0f, -9.0f, 9.0f, -1.0f, 1.0f));
 
-	/*
+
+	
 	int iterations = 0;
-	float step = 0.5f;
+	float step = 2.0f;
 	for (float y = -9.0f; y < 9.0f; y += step)
 		for (float x = -16.0f; x < 16.0f; x += step)
 		{
@@ -50,15 +51,16 @@ int main(int argc, char* argv[])
 		}
 
 	std::cout << "Made " << iterations << " sprites" << std::endl;
-	*/
+	
 
-	glm::mat4 matrix(1.0);
+	/*glm::mat4 matrix(1.0);
 	matrix[3][0] = -5;
 	matrix[3][1] = -7;
 	Group* group = new Group(matrix);
 	group->add(new Sprite(glm::vec3(0, 0, 0), glm::vec2(7, 7), TextureManager::getTexture("face")));
+	group->add(new Sprite(glm::vec3(2, 2, 0), glm::vec2(7, 7), TextureManager::getTexture("whocares")));
 
-	layer.add(group);
+	layer.add(group);*/
 
 	while (!window.shouldClose())
 	{
